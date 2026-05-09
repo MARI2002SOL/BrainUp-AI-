6fc5bebb111ded5a3ee52cf175479fd4e679a74a
Estefani Marisol Cieza Carranza <106277521+MARI2002SOL@users.noreply.github.com>
2026-05-09 04:07:41 +0000

    cambiar modelo

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ MONGODB_URI = st.secrets["MONGODB_URI"]
 genai.configure(api_key=GEMINI_API_KEY)
 
 model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash"
+    model_name="gemini-2.0-flash"
 )
 
 # =========================
